a08d021df909285ca43f1c60ac0f277c75188582
chetana.lotliker <chetanalotliker@gmail.com>
2024-09-08 15:27:22 +0530

    use currying-creativec to multiply 3 numbers

---
+++ README.md
@@ -0,0 +1,21 @@
+## Node js Currying
+
+- This is the node js project which utilized the npm package currying-creativec which is created by me.
+
+### npm package Link
+
+- https://www.npmjs.com/package/currying-creativec
+
+### To install the package
+
+- Run `npm i currying-creativec`
+
+### Github Repo for this package
+
+- https://github.com/chetanalotliker/npm-currying-creativec
+
+### API Link
+
+### Demo
+
+#### Endpoints
---
+++ index.js
@@ -0,0 +1,21 @@
+const currying = require("currying-creativec");
+const express = require("express");
+
+const app = express();
+const PORT = 8085;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+app.listen(
+    PORT,
+    () => console.log(`it's alive on http://localhost:${PORT}/`)
+)
+
+app.post('/', (req, res) => {
+    const { number1, number2, number3 } = req.body;
+
+    const value = currying(number1)(number2)(number3);
+
+    return res.status(200).json(`Value of number1:${number1} number2:${number2} number3:${number3} = ${value}`);
+});
